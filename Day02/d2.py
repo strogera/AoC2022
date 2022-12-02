29b530bf6320320d15088dce083468aff275cae3
strogera <26325577+strogera@users.noreply.github.com>
2022-12-02 16:51:20 +0200

    small change

--- Day02/d2.py
+++ Day02/d2.py
@@ -1,14 +1,13 @@
-shapeScore = {'A': 1, 'B': 2, 'C': 3}
 outcomeScore = {
-    ('A', 'A'): 3,
-    ('A', 'B'): 6,
-    ('A', 'C'): 0,
-    ('B', 'A'): 0,
-    ('B', 'B'): 3,
-    ('B', 'C'): 6,
-    ('C', 'A'): 6,
-    ('C', 'B'): 0,
-    ('C', 'C'): 3
+    ('A', 'A'): 1 + 3,
+    ('A', 'B'): 2 + 6,
+    ('A', 'C'): 3 + 0,
+    ('B', 'A'): 1 + 0,
+    ('B', 'B'): 2 + 3,
+    ('B', 'C'): 3 + 6,
+    ('C', 'A'): 1 + 6,
+    ('C', 'B'): 2 + 0,
+    ('C', 'C'): 3 + 3
 }
 userShape1 = {'X': 'A', 'Y': 'B', 'Z': 'C'}
 userShape2 = {
@@ -23,8 +22,8 @@ userShape2 = {
     ('C', 'Z'): 'A'
 }
 
-getScore1 = lambda a, b: shapeScore[userShape1[b]] + outcomeScore[a, userShape1[b]]
-getScore2 = lambda a, b: shapeScore[userShape2[(a, b)]] + outcomeScore[a, userShape2[(a, b)]]
+getScore1 = lambda a, b: outcomeScore[a, userShape1[b]]
+getScore2 = lambda a, b: outcomeScore[a, userShape2[(a, b)]]
 
 with open("input.txt", "r") as inputFile:
     play = [(x, y) for x, y in (line.strip().split(' ') for line in inputFile.readlines())]
