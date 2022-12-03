6a1dca76c79aa01f50dd6fa47116c877ba9e034a
strogera <26325577+strogera@users.noreply.github.com>
2022-12-03 20:11:06 +0200

    small code improvements

--- Day03/d3.hs
+++ Day03/d3.hs
@@ -7,13 +7,11 @@ main = do
     print $  part1 input
     print $  part2 input
 
-part1 input = sum $ score <$> map (\x ->  head $ uncurry intersect $ splitAt (length x `div` 2)  x) input
+part1 input = sum $ score <$> map (\x -> head $ uncurry intersect $ splitAt (length x `div` 2) x) input
+part2 input 
+    | null input = 0
+    | otherwise = score (head $ foldl1 intersect $ take 3 input) + part2 (drop 3 input)
 
-part2:: [[Char]] -> Int
-part2 [] = 0
-part2 input = score (head $ foldl1 intersect $ take 3 input) + (part2 $ drop 3 input)
-
-score:: Char -> Int
 score c 
     | isLower c = ord c - ord 'a' + 1
     | otherwise = ord c - ord 'A' + 27
