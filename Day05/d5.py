0d454734952ba9d79d2f65a586e39008810efc9b
strogera <26325577+strogera@users.noreply.github.com>
2022-12-05 17:11:23 +0200

    clean up

--- Day05/d5.py
+++ Day05/d5.py
@@ -1,18 +1,17 @@
 with open("input.txt", "r") as inputFile:
     stacks = [[] for _ in range(10)]
     inputStacks, inputCmds = inputFile.read().split("\n\n")
-    for x in inputStacks.split('\n')[:-1]:
-        l = x.replace('[', ' ').replace(']', ' ')
-        for i in range(1, len(l), 4):
-            if(l[i] != ' '):
-                stacks[i//4 + 1].append(l[i])
+    for line in inputStacks.split('\n')[:-1]:
+        for i in range(1, len(line), 4):
+            if(line[i] != ' '):
+                stacks[i//4 + 1].append(line[i])
     stacks2 = [x.copy() for x in stacks]
     for x in (y.replace('move ', '').replace('from ', '').replace('to ', '') for y in inputCmds.split('\n')):
         n, src , dest = map(int, x.split(' '))
-        items = [stacks[src].pop(0) for _ in range(n)]
-        items2 = [stacks2[src].pop(0) for _ in range(n)]
-        stacks[dest] =  items[::-1] + stacks[dest]
-        stacks2[dest] =  items2 + stacks2[dest]
+        stacks[dest] = stacks[src][:n][::-1] + stacks[dest]
+        stacks2[dest] = stacks2[src][:n] + stacks2[dest]
+        stacks[src] = stacks[src][n:]
+        stacks2[src] = stacks2[src][n:]
     getAns = lambda x: ''.join([x[0] for x in filter(lambda k: len(k) > 0, x)])
     print(getAns(stacks))
     print(getAns(stacks2))
