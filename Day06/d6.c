fae682b01134e7d68beb9ed15dbe31217319160d
strogera <26325577+strogera@users.noreply.github.com>
2022-12-06 19:55:16 +0200

    Day 6 c optimized

---
+++ Day06/d6.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define L1 4u
+#define L2 14u
+
+inline uint32_t countOnes(uint32_t x)
+{
+    x = x - ((x >> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+    x = (x + (x >> 4)) & 0x0F0F0F0F;
+    x = x + (x >> 8);
+    x = x + (x >> 16);
+    return x & 0x0000003F;
+}
+
+int main()
+{
+    FILE *f = fopen("input.txt", "r");
+    char cur[L1];
+    memset(&cur, getc(f), sizeof(char) * L1);
+    char cur2[L2];
+    int i = 1;
+    uint32_t x = 0u;
+    while (x != L1)
+    {
+        x = 0u;
+        cur[i++ % L1] = getc(f);
+        for (int j = 0; j < L1; ++j)
+        {
+            x |= 1u << cur[j] % 32;
+        }
+        x = countOnes(x);
+    }
+    printf("%d\n", i);
+    for (int j = i; (i - j) < L1; --j)
+    {
+        cur2[j % L2] = cur[i - j];
+    }
+    while (x != L2)
+    {
+        x = 0u;
+        cur2[i++ % L2] = getc(f);
+
+        for (int j = 0; j < L2; j++)
+        {
+            x |= 1u << cur2[j] % 32;
+        }
+        x = countOnes(x);
+    }
+    printf("%d\n", i);
+    fclose(f);
+}
